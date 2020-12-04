c53d5990bddcf10b41eb7079685f85f486fa056a
konstantin55000 <pkonstantin55000@gmail.com>
2020-12-04 22:25:00 +0200

    toolbar changes

--- index.js
+++ index.js
@@ -169,10 +169,7 @@ blockManager.creatingNewBlock('h2-block', {
 });
 // Testing creating new block without options
 blockManager.creatingNewBlock('h3-block', { label: 'Heading' }, 'blocks');
-
-
-
-
+  
 editor.Panels.addButton('devices-c', [ { id: 'toggle-panel-right3', className: 'fa fa-arrows-alt icon-blank', 
  command: {
   run: function(editor) {
@@ -182,8 +179,9 @@ editor.Panels.addButton('devices-c', [ { id: 'toggle-panel-right3', className: '
   stop: function(editor) {
     editor.setDragMode('block'); 
   }
-  
-}     } ] );
+}
+
+} ] );
  
 editor.Panels.addButton('devices-c', [ { id: 'toggle-panel-right2', className: 'fa fa-plus  icon-blank',
  command: function(editor1, sender) {
@@ -211,109 +209,62 @@ editor.Commands.extend('core:fullscreen', {
   }
 });
 
-//allow components to be drag and dropped
-// editor.setDragMode('absolute');
-
-//editor.on('storage:start:store', (objectToStore) => {
-//  console.log("Extend parameters to store");
-//  console.log(pageManager.pgs + ":" + pageManager.crrPg);
-//  objectToStore.pageId = pageManager.pgs[pageManager.crrPg].id;
-//  objectToStore.pageName = pageManager.pgs[pageManager.crrPg].name;
-//  objectToStore.userProjectId = getUrlVars()["userProject"];
-//});
-
-// Left-side panel with page list
-const leftBar = document.getElementsByClassName('left-bar')[0];
+ 
 const rightBar = document.getElementsByClassName('right-bar')[0];
-//const pageManagerDOM = document.getElementsByClassName('page-manager-body')[0];
-
+ 
+  
 //hide left panel on preview
 editor.on('run:preview:before', () => {
-  leftBar.style.display = 'none';
+ 
   rightBar.style.left = '0';
   rightBar.style.setProperty("width", "100%", "important");
 });
 
 editor.on('stop:preview:before', () => {
-  leftBar.style.display = '';
+ 
   rightBar.style.left = '13.04%';
   rightBar.style.setProperty("width", "calc(100% - 13.04%) ", "important");
 });
 
  
-editor.on('component:selected', () => {
+editor.on('component:selected', (model) => {
 
     // whenever a component is selected in the editor 
+    let self = this;
+    const freeModeCommand = () => {
+      model.set('dmode', 'absolute');
+    }
 
-    // set your command and icon here
-    const toggleDropDown = () => { 
-      console.log('test: ', jQuery('.gpd-settings-panel').length ); 
-      if ( jQuery('.gpd-settings-panel').length == 0 ) {
-            let toolbarString =`<div class="gpd-settings-panel" >
-            <div class="gpd-settings-panel__option" >Select parent</div>
-            <div class="gpd-settings-panel__option">Create block</div>
-            <div class="gpd-settings-panel__option">Toggle Free Move</div>
-          </div>`; 
-        jQuery(toolbarString).appendTo(jQuery('.gjs-toolbar'));
-      } 
-    };
-
+    const addBlock = () => {
+      alert('add new block');
+    }
     const selectedComponent = editor.getSelected();
-    const commandIcon = 'fa fa-cog';  
+  
     const defaultToolbar = selectedComponent.get('toolbar');
-    const commandExists = defaultToolbar.some(item => item.command === toggleDropDown ); 
+    const commandExists = defaultToolbar.some(item => item.command === freeModeCommand ); 
+    //model.set('dmode', 'absolute'); 
+    // self.setAttribute('gis-free-mode',"1"); //not a function
+    //selectedComponent.setAttribute('gis-free-mode',"1"); //not a function 
+    console.log(self, selectedComponent);
+    
+    
+    const addBlockClass = 'fa fa-plus'; 
+    const iconFreeMode = 'fa fa-arrows-alt';
     if (!commandExists) {
       selectedComponent.set({
-        toolbar: [ ...defaultToolbar, {  attributes: {class: commandIcon}, command:  toggleDropDown }
+        toolbar: [ ...defaultToolbar, {  attributes: {class: iconFreeMode  }, command:  freeModeCommand  },
+            {  attributes: {class: addBlockClass }, command:  addBlock },
+       
         ]
       });
+
     }
+  
       
   });
 
 
   
-
- 
-
-
-//const data = {userProjectId: getUrlVars()["userProject"]};
-//editor.Commands.add('canvas-publish', e => {
-//      fetch('http://localhost:9080/cadau/user-project/publish', {//url not found
-//        method: 'POST',
-//        body: JSON.stringify(data),
-//      })
-//      .then(response => response.json())
-//      .then(data => {
-//        alert("success: " + data);
-//      });
-//});
-
-//// Create and add page (name and controls) to DOM
-//function crtAddPgVw (name, i, flag) {
-//  const el = document.createElement('DIV');
-//  el.classList.add('left-bar__item', 'bar-item');
-//  const fw = flag ? 'item-bold' : '';
-//  let inner = `<span class="page-manager-body__item-title inline-button ${fw}" onclick="pageManager.chnCrrPg(${i})">${name}</span>`;
-//  inner += `<i class="page-manager-body__item-control inline-button fa fa-copy" onclick="pageManager.cpPg(${i})"></i>`;
-//  inner += `<i class="page-manager-body__item-control inline-button fa fa-edit" onclick="pageManager.rnmPg(${i})"></i>`;
-//  inner += `<i class="page-manager-body__item-control inline-button fa fa-trash" onclick="pageManager.dltPg(${i})"></i>`;
-//  el.innerHTML = inner;
-//  pageManagerDOM.appendChild(el);
-//}
-
-// Clearing panel and filling it by pages
-//function drwNmsPgs (pgs, cur) {
-//  let lstChl = pageManagerDOM.lastElementChild;
-//  while (pageManagerDOM.childElementCount > 1) {
-//    pageManagerDOM.removeChild(lstChl);
-//    lstChl = leftBar.lastElementChild;
-//  }
-//  for (let i = 0; i < pgs.length; i++) {
-//    crtAddPgVw(pgs[i].name, i, i === cur);
-//  }
-//}
-
 function getUrlVars() {
   let vars = {};
   let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
@@ -322,94 +273,7 @@ function getUrlVars() {
   return vars;
 }
 
-// Object contains interactions with pages logic
-//const pageManager = {
-//  // Default page
-//  dflPg: {
-//    components: null,
-//    styles: null,
-//  },
-//  // List of pages
-//  pgs: [
-//
-//  ],
-//  // Index of current page
-//  crrPg: 0,
-//  // Change index of current page to i
-//  chnCrrPg: function (i) {
-//    if (i !== this.crrPg) {
-//      editor.store();
-//      if (this.crrPg !== -1) {
-//        this.pgs[this.crrPg].components = editor.getHtml();
-//        this.pgs[this.crrPg].styles = editor.getCss();
-//      }
-//      this.crrPg = i;
-//      editor.setComponents(i !== -1 ? this.pgs[this.crrPg].components : '');
-//      editor.setStyle(i !== -1 ? this.pgs[this.crrPg].styles : '');
-//      drwNmsPgs(this.pgs, this.crrPg);
-//    }
-//  },
-//  // Add new default page to list
-//  addNewPg: function () {
-//    const str = prompt('Page name', this.pgs[this.crrPg].name);
-//    if (str !== '' && str !== undefined && str !== null) {
-//      this.pgs.push({
-//        id: null,
-//        name: str,
-//        components: this.dflPg.components,
-//        styles: this.dflPg.styles,
-//        externalStyles: null,
-//        externalScripts: null
-//      });
-//      drwNmsPgs(this.pgs, this.crrPg);
-//
-//      //load the newly created page
-//      this.chnCrrPg(this.pgs.length-1);
-//    }
-//  },
-//  // Copy of existing page having index i
-//  cpPg: function (i) {
-//    const pg = this.pgs[i];
-//    if (this.crrPg === i) {
-//      this.pgs[this.crrPg].components = editor.getHtml();
-//      this.pgs[this.crrPg].styles = editor.getCss();
-//    }
-//    this.pgs.push({
-//      id: null,
-//      name: pg.name + '_copy',
-//      components: pg.components,
-//      styles: pg.styles,
-//      externalStyles: null,
-//      externalScripts: null
-//    });
-//    drwNmsPgs(this.pgs, this.crrPg);
-//
-//    //load the newly created page
-//    this.chnCrrPg(this.pgs.length-1);
-//  },
-//  // Rename page having index i
-//  rnmPg: function (i) {
-//    const str = prompt('Rename the page', this.pgs[i].name);
-//    if (str !== '' && str !== undefined && str !== null) {
-//      this.pgs[i].name = str;
-//      drwNmsPgs(this.pgs, this.crrPg);
-//    }
-//  },
-//  // Delete page having index i
-//  dltPg: function (i) {
-//    if (confirm(`Do you want to delete ${this.pgs[i].name} ?`)) {
-//      this.chnCrrPg(i - 1);
-//      this.pgs.splice(i, 1);
-//      drwNmsPgs(this.pgs, this.crrPg);
-//    }
-//  },
-//  // Get array with html and css of all pages
-//  getData() {
-//    this.pgs[this.crrPg].components = editor.getHtml();
-//    this.pgs[this.crrPg].styles = editor.getCss();
-//    return this.pgs;
-//  }
-//};
+
 
 // Init default page by editor content
 window.onload = function (event) {
@@ -428,44 +292,5 @@ window.onload = function (event) {
   editor.Panels.getButton('views', 'open-sm').set('active', true);
   // Open a panel
   editor.Commands.run('open-sm');
-
-//  const Http = new XMLHttpRequest();
-//  const url='/cadau/user-project/1/pages';
-//  Http.open("GET", url);
-//  Http.send();
-//
-//  Http.onreadystatechange = function() {
-//    if(this.readyState == 4 && this.status == 200) {
-//      let response = JSON.parse(Http.responseText);
-//      for(let i=0; i<response.length; i++) {
-//        let jsonObj = response[i];
-//        let pageObj = {};
-//
-//        pageObj.id = jsonObj.id;
-//        pageObj.name = jsonObj.name;
-//        pageObj.components = jsonObj.html;
-//        pageObj.styles = jsonObj.css;
-//        pageObj.externalStyles = jsonObj.externalStyles;
-//        pageObj.externalScripts = jsonObj.externalScripts;
-//
-//        pageManager.pgs.push(pageObj);
-//      }
-//
-//      drwNmsPgs(pageManager.pgs, pageManager.crrPg);
-//
-//      editor.setComponents(pageManager.pgs[0].components);
-//      editor.setStyle(pageManager.pgs[0].styles);
-//
-//      for(let i=0; i<pageManager.pgs[0].externalStyles.length; i++) {
-//        let link = pageManager.pgs[0].externalStyles[i];
-//        editor.Canvas.getFrame().addLink(link);
-//      }
-//
-//      for(let i=0; i<pageManager.pgs[0].externalScripts.length; i++) {
-//        let script = pageManager.pgs[0].externalScripts[i];
-//        editor.Canvas.getFrame().addScript(script);
-//      }
-//
-//    }
- // }
+ 
 };
