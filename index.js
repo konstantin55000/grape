ce4844f6b5d7a81813a54444198b6a39820e0419
konstantin55000 <pkonstantin55000@gmail.com>
2020-12-09 20:29:16 +0200

    fix

--- index.js
+++ index.js
@@ -1,3 +1,132 @@
+//Class
+class CssRules {
+
+run(editor, snd, opts = {}) {
+  const component = opts.target || editor.getWrapper();
+  const cssc = editor.CssComposer;
+  const rules = cssc.getAll();
+  let result = '';
+
+  const { atRules, notAtRules } = this.splitRules(this.matchedRules(component, rules));
+  notAtRules.forEach(rule =>  result += rule.toCSS());
+  this.sortMediaObject(atRules).forEach(item => {
+    let rulesStr = '';
+    const atRule = item.key;
+    const mRules = item.value;
+
+    mRules.forEach(rule => {
+      const ruleStr = rule.getDeclaration();
+
+      if (rule.get('singleAtRule')) {
+        result += `${atRule}{${ruleStr}}`;
+      } else {
+        rulesStr += ruleStr;
+      }
+    });
+
+    if (rulesStr) result += `${atRule}{${rulesStr}}`;
+  });
+
+  return result;
+}
+
+/**
+ * Get matched rules of a component
+ * @param {Component} component
+ * @param {Array<CSSRule>} rules
+ * @returns {Array<CSSRule>}
+ */
+matchedRules(component, rules) {
+  const el = component.getEl();
+  let result = [];
+
+  rules.forEach(rule => {
+    try {
+      if (rule.selectorsToString().split(',').some(
+        selector => el.matches(this.cleanSelector(selector))
+      )) {
+        result.push(rule);
+      }
+    } catch (err) {}
+  });
+
+  component.components().forEach(component => {
+    result = result.concat(this.matchedRules(component, rules))
+  });
+
+  // Remove duplicates
+  result = result.filter((rule, i) => result.indexOf(rule) == i);
+
+  return result;
+}
+
+/**
+ * Return passed selector without states
+ * @param {String} selector
+ * @returns {String}
+ */
+cleanSelector(selector) {
+  return selector.split(' ').map(item => item.split(':')[0]).join(' ');
+}
+
+/**
+ * Split an array of rules in atRules and not
+ * @param {Array<CSSRule>} rules
+ * @returns {Object}
+ */
+splitRules(rules) {
+  const atRules = {};
+  const notAtRules = [];
+
+  rules.forEach(rule => {
+    const atRule = rule.getAtRule();
+
+    if (atRule) {
+      const mRules = atRules[atRule];
+
+      if (mRules) {
+        mRules.push(rule);
+      } else {
+        atRules[atRule] = [rule];
+      }
+    } else {
+      notAtRules.push(rule);
+    }
+  });
+
+  return {
+    atRules,
+    notAtRules,
+  };
+}
+
+/**
+ * Get the numeric length of the media query string
+ * @param  {String} mediaQuery Media query string
+ * @return {Number}
+ */
+getQueryLength(mediaQuery) {
+  const length = /(-?\d*\.?\d+)\w{0,}/.exec(mediaQuery);
+  if (!length) return Number.MAX_VALUE;
+
+  return parseFloat(length[1]);
+}
+
+/**
+ * Return a sorted array from media query object
+ * @param  {Object} items
+ * @return {Array}
+ */
+sortMediaObject(items = {}) {
+  const itemsArr = [];
+ _.each(items, (value, key) => itemsArr.push({ key, value }));
+  return itemsArr.sort(
+    (a, b) => this.getQueryLength(b.key) - this.getQueryLength(a.key)
+  );
+}
+
+}; 
+
 // Grapes-editor object
 const editor = grapesjs.init({
   height: '100%',
@@ -274,7 +403,7 @@ editor.on('stop:preview:before', () => {
 
 var drModeIsOn = false;
 editor.on('component:selected', (model) => {
- 
+  console.log('sel comp css',   editor.getSelectedToStyle().attributes);
     // whenever a component is selected in the editor
     let self = this; 
     setTimeout(()=>{
@@ -347,13 +476,13 @@ function getUrlVars() {
   editorTextArea.value = localStorage.getItem('editorTextArea_' + cid);
   cssTextArea.value = localStorage.getItem('cssTextArea_' + cid);
   editorTextBlockName.value  = localStorage.getItem( 'blockName_' + cid );
-  editorTextCategoryName.value = localStorage.getItem( 'catValue_' + cid );
-
+  editorTextCategoryName.value = localStorage.getItem( 'catValue_' + cid ); 
   let contentToSet = editorTextArea.value + ' ' + cssTextArea.value;
-  codeViewer.setContent(contentToSet);
-
+  codeViewer.setContent(contentToSet); 
 }
 
+
+ 
 // Add new block
 editor.Commands.add("open-html-code-editor", {
   run: function (editor, sender, data) {
@@ -470,9 +599,17 @@ editor.Commands.add("open-html-code-editor", {
       //if not from tab, get for select component.
       var selComponent = editor.getSelected();
       var attr = editor.getSelectedToStyle().attributes;
-      Css = attr.style;
+      //was: Css = attr.style;
+      let cssSpliter = new CssRules();
+      cssString = cssSpliter.run(editor);
+
+      console.log(cssString);
+      //alert(cssString);
+
       htmlString = editor.getSelected().toHTML();
+
       editorTextArea.innerHTML = htmlString;
+
     } else {
       var customBlock = `<div class="my-block-class">
               My new block example
@@ -489,13 +626,7 @@ editor.Commands.add("open-html-code-editor", {
     }
 
     var cssTextArea = document.querySelector('[name="css"]');
-    if (selectedComponent) {
-      cssString = "{ ";
-      for (const [key, value] of Object.entries(Css)) {
-        cssString += key + ": " + value + ";";
-      }
-      cssString += " }";
-    }
+   
 
     cssTextArea.innerHTML = cssString;
     codeViewer.init(editorTextArea);
@@ -580,6 +711,7 @@ editor.Commands.add("open-html-code-editor", {
       codeViewer.editor.refresh();
       codeViewerCss.editor.refresh();
       setIframeContent(cssString, htmlString);
+
     },1);
   },
 });
